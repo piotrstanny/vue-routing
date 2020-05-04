0346ae0b79b08e5f9834ecd5db61331904f0351d
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-04 21:55:25 +0100

    Router imported and raw navbar implemented

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
+        <div class="row justify-content-center">
+            <div class="col-xs-12 col-sm-8">
                 <h1>Routing</h1>
+                <hr>
+                <Header/>
+                <router-view></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Header from './Header.vue'
     export default {
+        components: {
+            Header
+        }
     }
 </script>
 
 <style>
+.container {
+    margin-top: 50px;
+}
 </style>
